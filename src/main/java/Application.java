83dc55b9acef864d235b1c836d3195bd5d150b4c
benmuratseker <benmuratseker@hotmail.com>
2024-12-13 14:28:07 +0100

    app name refactored and run config added

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,7 @@
-package com.xdlab;
-
 import com.xdlab.service.ISpeakerService;
 import com.xdlab.service.SpeakerService;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
 
         ISpeakerService service = new SpeakerService();
