c70bd8f066430e04aad17f7f880170091636760c
benmuratseker <benmuratseker@hotmail.com>
2024-12-15 00:04:58 +0100

    component scope autowire for service and repo

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,25 +4,30 @@ import com.xdlab.service.ISpeakerService;
 import com.xdlab.service.SpeakerService;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.xdlab"})
 public class AppConfig {
 
+    /*
     @Bean(name="speakerService")//as default concept is singleton
     //@Scope(value = "singleton")
-    //@Scope(value = BeanDefinition.SCOPE_SINGLETON)
-    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE) // unique instance per request
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE) // unique instance per request
     public ISpeakerService getSpeakerService(){
         //SpeakerService service = new SpeakerService(getSpeakerRepository());
         SpeakerService service = new SpeakerService();
         //service.setRepository(getSpeakerRepository());// @Autowired will do this automatically
         return service;
     }
+     */
 
+    /*
     @Bean(name = "speakerRepository")
     public ISpeakerRepository getSpeakerRepository(){
         return new StubSpeakerRepository();
-    }
+    }*/
 }
--- src/main/java/com/xdlab/service/SpeakerService.java
+++ src/main/java/com/xdlab/service/SpeakerService.java
@@ -3,9 +3,15 @@ package com.xdlab.service;
 import com.xdlab.model.Speaker;
 import com.xdlab.repository.ISpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
+@Scope(value = BeanDefinition.SCOPE_SINGLETON)
+//with these annotations we eliminate speakerService bean in config
 public class SpeakerService implements ISpeakerService {
 
     private ISpeakerRepository repository;
